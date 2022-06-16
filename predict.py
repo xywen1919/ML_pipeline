3e4f3ecf16821f926fccedfbe459f6da92b49b21
xywen1919 <xynwen@yahoo.com>
2022-06-16 19:38:08 -0400

    import data and linear model

--- predict.py
+++ predict.py
@@ -1,1 +1,24 @@
+#%%
+# load linear model and the corresponding test set
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+with open ('linear_model.pkl', 'rb') as file:
+  linear = pickle.load(file)
+
+with open ('./data/intermid/X_test_fs.pkl', 'rb') as file:
+  X_test_fs = pickle.load(file)
+
+with open ('./data/intermid/y_test.pkl', 'rb') as file:
+  y_test = pickle.load(file)
+
+# evaluate the model
+y_pred = linear.predict(X_test_fs)
+# evaluate predictions
+sns.lineplot(y_test, y_pred)
+plt.xlabel('True rent price')
+plt.ylabel('Predicted price')
+plt.show()
+
+# %%
